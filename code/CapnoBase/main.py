5cac5f9e71734c70c83d6fad28312b4ce3110e42
simcak <peta.simcak@gmail.com>
2025-02-03 21:29:30 +0100

    sensitivity, specificity, accuracy added

--- code/CapnoBase/main.py
+++ code/CapnoBase/main.py
@@ -1,4 +1,5 @@
 from mypackage import data, preprocess, show, peaks, calcul
+import numpy as np
 # from refpackage import aboy, elgendi
 
 def main_calculation(capnobase_file, show_plots=False):
@@ -15,19 +16,30 @@ def main_calculation(capnobase_file, show_plots=False):
 		show.two_signals_peaks(ppg_signal, filtered_ppg_signal, ref_peaks, our_peaks, capnobase_file)
 		# show.two_signals(ppg_signal, filtered_ppg_signal, capnobase_file)
 
-	return ref_hr, our_hr
+	return ref_hr, our_hr, ref_peaks, our_peaks
 
 def main():
 	capnobase_files = data.list_of_files()
+	sensitivity = []
+	specificity = []
+	accuracy = []
 
 	for i in range(len(capnobase_files)):
 		print(f'{i})', end=' ')
-		ref_hr, our_hr = main_calculation(capnobase_files[i], show_plots=False)
+		ref_hr, our_hr, ref_peaks, our_peaks = main_calculation(capnobase_files[i], show_plots=False)
+
+		# Sensitivity, specificity, and accuracy
+		sensitivity.append(len(set(ref_peaks) & set(our_peaks)) / len(ref_peaks))
+		specificity.append(len(set(ref_peaks) & set(our_peaks)) / len(our_peaks))
+		accuracy.append(1 - np.mean(np.abs(ref_hr - our_hr) / ref_hr))
 
 	# For testing purposes
 	print('\n')
 	file_index = 2
-	ref_hr, our_hr = main_calculation(capnobase_files[file_index], show_plots=True)
+	ref_hr, our_hr, ref_peaks, our_peaks = main_calculation(capnobase_files[file_index], show_plots=True)
+
+	print('\n')
+	print(f'Sensitivity: {np.mean(sensitivity)} | Specificity: {np.mean(specificity)} | Accuracy: {np.mean(accuracy)}')
 
 if __name__ == "__main__":
 	main()
